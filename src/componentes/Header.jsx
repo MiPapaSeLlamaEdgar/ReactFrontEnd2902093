735225acd3e5b6864cc6c819f8b2ee1d188fe420
Steven <davidm435465123@gmail.com>
2024-10-23 10:41:53 -0500

    primer commit

---
+++ src/componentes/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Header = ({titulo, autor, centro, ficha}) => {
+
+    const headerStyles = {
+        backgroundColor: 'rgba(0,0,0,0.4)',
+        color: '#045abc',
+    },
+    titleStyles = {
+        color: "green"
+    },
+    autorStyles = {
+        color: 'blue'
+    },
+    centroStyles = {
+        color: 'red'
+    },
+    fichaStyles = {
+        color: 'yellow'
+    }
+
+
+
+
+  return (
+    <header style={headerStyles}>
+            <p style={titleStyles}>{titulo}</p>
+            <p style={autorStyles}>Autor: {autor} </p>
+            <p style={centroStyles}>Centro de formacion: {centro}</p>
+            <p style={fichaStyles}>Ficha: {ficha}</p>
+    </header>
+  )
+}
+
+export default Header
